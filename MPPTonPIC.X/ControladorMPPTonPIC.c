cb9926c1605380f0b46bed8d461da89d8621d5c0
vladi29 <42014792+vladi29@users.noreply.github.com>
2020-08-09 20:39:47 -0400

    Proyecto en MPLAB
    Creado el proyecto en MPLAB para poder compilar el código que irá hacia el PIC

---
+++ MPPTonPIC.X/ControladorMPPTonPIC.c
@@ -0,0 +1,13 @@
+/*
+ * File:   ControladorMPPTonPIC.c
+ * Author: Vladimir Alfaro
+ *
+ * Created on August 9, 2020, 8:38 PM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
---
+++ MPPTonPIC.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=MPPTonPIC.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/MPPTonPIC.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=mpptonpic.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/mpptonpic.x.tar
